9ec3ed53620741f3c5d9788e9f176c28f2a0fa25
Matheus Mignoni <matheus.mignoni@poatek.com>
2019-11-28 00:16:09 -0300

    Chat cache added

---
+++ Services/ChatCache/ChatCache.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.ChatCache
+{
+    public class ChatCache
+    {
+        public User GetConnectedUserByName(string name)
+        {
+
+        }
+        public User GetConnectedUserByConnectionId(string connectionId)
+        {
+
+        }
+        public bool TryAddUser(User user)
+        {
+
+        }
+        public bool RemoveUserByConnectionId(string connectionId)
+        {
+
+        }
+        public bool RemoveUserByName(int name)
+        {
+
+        }
+    }
+}
---
+++ Services/ChatCache/IChatCache.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.ChatCache
+{
+    public interface IChatCache
+    {
+        User GetConnectedUserByName(string name);
+        User GetConnectedUserByConnectionId(string connectionId);
+        bool TryAddUser(User user);
+        bool RemoveUserByConnectionId(string connectionId);
+        bool RemoveUserByName(int name);
+    }
+}
