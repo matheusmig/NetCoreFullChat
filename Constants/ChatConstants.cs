d0770ff7b839ca86f9ec27e6a6c4202b7d6a0cf5
Matheus Mignoni <matheus.mignoni@poatek.com>
2019-11-28 00:16:21 -0300

    Chat Constants

---
+++ Constants/ChatConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Constants
+{
+    public class ChatConstants
+    {
+        public static string ChatHubPath = "/chatHub";
+
+        public static string DateFormatString = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.ffffff";
+    }
+}
